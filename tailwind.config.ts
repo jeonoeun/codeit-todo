f6865d09daa5ce3d3806d82af25c29838ed9140d
Jeongeun Lee <zaekoch@gmail.com>
2025-02-07 00:16:59 +0900

    💄style: 컬러 시스템 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,28 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        slate: {
+          900: "#0F172A",
+          800: "#1E293B",
+          500: "#64748B",
+          400: "#94A3B8",
+          300: "#CBD5E1",
+          200: "#E2E8F0",
+          100: "#F1F5F9",
+        },
+        violet: {
+          600: "#7C3AED",
+          100: "#EDE9FE",
+        },
+        rose: {
+          500: "#F43F5E",
+        },
+        lime: {
+          300: "#BEF264",
+        },
+        amber: {
+          800: "#92400E",
+        },
       },
     },
   },
