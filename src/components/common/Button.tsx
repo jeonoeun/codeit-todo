b968521e3812ccf2ddf4f8ec60dcef22e24377ec
Jeongeun Lee <zaekoch@gmail.com>
2025-02-07 23:55:19 +0900

    ♻️refactor: Button 컴포넌트 스타일 및 구조 변경

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -4,30 +4,35 @@ import Image, { StaticImageData } from "next/image";
 import clsx from "clsx";
 
 interface ButtonProps {
-  type: "submit" | "button";
+  type?: "submit" | "button";
   text?: string;
-  icon: StaticImageData;
+  icon?: StaticImageData;
   variant: "slate" | "violet" | "rose" | "lime" | "dark";
   border?: boolean;
 }
 
-const Button = ({ type, text, icon, variant, border }: ButtonProps) => {
+const Button = ({
+  type = "button",
+  text,
+  icon,
+  variant,
+  border,
+}: ButtonProps) => {
   return (
     <button
       type={type}
       onClick={() => {}}
       className={clsx(
         "btn-base",
-        `btn-${variant}`,
-        type === "submit"
-          ? "w-[168px] h-[56px] shadow-[4px_4px_0px_#0F172A]"
-          : "w-[64px] h-[64px]",
-        text && "gap-1",
+        `btn-${variant} btn-${type}`,
+        text ? "gap-1" : "",
         border && "border-2 border-slate-900"
       )}
     >
-      <Image src={icon} alt={text ? `${text} 아이콘` : "버튼 아이콘"} />
-      {text}
+      {icon && (
+        <Image src={icon} alt={text ? `${text} 아이콘` : "버튼 아이콘"} />
+      )}
+      {text && <span className="hidden tablet:block">{text}</span>}
     </button>
   );
 };
